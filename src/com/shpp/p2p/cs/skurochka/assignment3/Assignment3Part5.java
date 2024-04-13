aacab987048edbab96de5d6416a6ce03472f78dc
Sergey90K <sergejwinlamerasus@gmail.com>
2024-04-14 00:10:08 +0300

    Add task 5

--- src/com/shpp/p2p/cs/skurochka/assignment3/Assignment3Part5.java
+++ src/com/shpp/p2p/cs/skurochka/assignment3/Assignment3Part5.java
@@ -1,4 +1,27 @@
 package com.shpp.p2p.cs.skurochka.assignment3;
 
-public class Assignment3Part5 {
+import com.shpp.cs.a.console.TextProgram;
+import java.util.Random;
+
+public class Assignment3Part5 extends TextProgram {
+    public void run() {
+        startGame();
+    }
+
+    private void startGame() {
+        Random randomGenerator = new Random();
+        int casinoMoney = 1, userMoney = 0, userGameCounter = 0;
+        while (userMoney <= 20) {
+            if ((randomGenerator.nextInt(2)) == 0) {
+                casinoMoney += casinoMoney;
+            } else {
+                userGameCounter++;
+                System.out.println("This game, you earned $" + casinoMoney);
+                userMoney += casinoMoney;
+                casinoMoney = 1;
+                System.out.println("Your total is $" + userMoney);
+            }
+        }
+        System.out.println("It took " + userGameCounter + " games to earn $20");
+    }
 }
